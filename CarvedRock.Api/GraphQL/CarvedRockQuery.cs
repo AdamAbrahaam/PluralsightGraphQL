3c2ca04a4dc2157a5e17c97fa1b87c6ef0d977fb
Adam Abrahám <xabrah04@vutbr.cz>
2019-08-19 12:37:31 +0200

    Review query

--- CarvedRock.Api/GraphQL/CarvedRockQuery.cs
+++ CarvedRock.Api/GraphQL/CarvedRockQuery.cs
@@ -6,7 +6,7 @@ namespace CarvedRock.Api.GraphQL
 {
     public class CarvedRockQuery : ObjectGraphType
     {
-        public CarvedRockQuery(ProductRepository productRepository)
+        public CarvedRockQuery(ProductRepository productRepository, ProductReviewRepository reviewRepository)
         {
             Field<ListGraphType<ProductType>>(      // I want to return a list => ListGraphType
                 "products",
@@ -21,6 +21,15 @@ namespace CarvedRock.Api.GraphQL
                 {
                     var id = context.GetArgument<int>("id");    // Get the specific ID from context
                     return productRepository.GetOne(id);
+                });
+
+            Field<ListGraphType<ProductReviewType>>(
+                "reviews",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> {Name = "productId"}),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<int>("productId");
+                    return reviewRepository.GetForProduct(id);
                 });
         }
     }
